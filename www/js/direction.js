95a26049c767c6260183ef675bee60095c60e9e6
lisitsya567 <120280978+lisitsya567@users.noreply.github.com>
2025-04-25 05:14:02 +0300

    Add files via upload

--- www/js/direction.js
+++ www/js/direction.js
@@ -1,19 +1,26 @@
 const swiper = new Swiper('.cards-doctors', {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    loop: false, // если нужно бесконечное листание — true
+    slidesPerView: 2,   // КОЛ-ВО КАРТОЧЕК ДЛЯ ПОКАЩА НА ЭКРАНЕ
+    spaceBetween: 20,  // ОТСТУП ОТ КАРТОЧКИ ПРОБЕЛ
+    slidesPerGroup: 1,  // КОЛ-ВО ПРОЛИСТЫВАЕМЫХ КАРТОЧЕК 
+    speed: 800, // СКОРОСТЬ ПЕРЕКЛЮЧЕНИЯ КАРТОЧЕК
+    // slideToClickedSlide: true, // ПЕРЕКЛЮЧЕНИЕ ПРИ КЛИКЕ НА КАРТОЧКУ 
+    loop: false, // БЕСКОНЕЧНОЕ ЛИСТАНИЕ НУЖНО true НЕ ПРОДДЕРДИВАЕТ СКРОЛЛ
     navigation: {
       nextEl: '.doctor-slider-btn-next',
       prevEl: '.doctor-slider-btn-prev',
     },
-    breakpoints: {
+    breakpoints: {   // АДАПТИВНОСТЬ НАСТРОЕК ПОД МОБИЛЬНЫЕ ЭКРАНЫ
       768: {
-        slidesPerView: 2,
+        slidesPerView: 1,
         spaceBetween: 20,
+        slidesPerGroup: 0.1,
+        touchRation: 1, // ЧУВСТВИТЕЛЬНОСТЬ СВАЙПА 
       },
       1024: {
         slidesPerView: 3,
-        spaceBetween: 30,
+        spaceBetween: 45,
+        slidesPerGroup: 1,
+        slideToClickedSlide: true,  
       }
     }
   });
